3e17ea0dfccb8ca959315346ee50c93f4e902951
wangdetash <wangdetash@gmail.com>
2025-03-29 16:02:01 +0530

    writing the video to a file

--- open_camera_vision.py
+++ open_camera_vision.py
@@ -1,12 +1,15 @@
 import cv2
 
 cap = cv2.VideoCapture(0) #can either provide path to file name or device index which can be 0 or -1
-
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out =  cv2.VideoWriter('output.avi',fourcc,20.0,(640,480)) #fourcc code is a four byte code which is used to specify the video codac. 20fps (640*480)  capture size
 while(cap.isOpened()): #checkin if the video can be accessed
     ret, frame = cap.read()
 
     print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    out.write(frame)#write the file 
 
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame',gray)
@@ -16,4 +19,5 @@ while(cap.isOpened()): #checkin if the video can be accessed
 
 
 cap.release()
+out.release()
 cap.destroyAllWindows()
