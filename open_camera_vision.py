9da7ff743628ed69568469e2372ed2e437fc7302
wangdetash <wangdetash@gmail.com>
2025-03-29 14:54:55 +0530

    using isOpened module to check whether video can be accessed.

--- open_camera_vision.py
+++ open_camera_vision.py
@@ -2,7 +2,7 @@ import cv2
 
 cap = cv2.VideoCapture(0) #can either provide path to file name or device index which can be 0 or -1
 
-while(True):
+while(cap.isOpened()): #checkin if the video can be accessed
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame',gray)
