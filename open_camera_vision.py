ccdd44344dc540d444ab7a3d4c7de8847a8fda70
wangdetash <wangdetash@gmail.com>
2025-03-29 16:25:11 +0530

    error correction for destroyAllWindows and printing frame size only once.

--- open_camera_vision.py
+++ open_camera_vision.py
@@ -1,13 +1,13 @@
 import cv2
 
 cap = cv2.VideoCapture(0) #can either provide path to file name or device index which can be 0 or -1
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out =  cv2.VideoWriter('output.avi',fourcc,20.0,(640,480)) #fourcc code is a four byte code which is used to specify the video codac. 20fps (640*480)  capture size
 while(cap.isOpened()): #checkin if the video can be accessed
     ret, frame = cap.read()
-
-    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     out.write(frame)#write the file 
 
@@ -20,4 +20,4 @@ while(cap.isOpened()): #checkin if the video can be accessed
 
 cap.release()
 out.release()
-cap.destroyAllWindows()
+cv2.destroyAllWindows()
